084645dbcd89f2dc5b0cb1a69a37f2d252f02cf1
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-03-13 02:11:50 +1300

    change controller to analyseCar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const upload = multer({dest: 'folderImages/'}); // Multer Shadow files location => Go to controller.js l-29 for details
 
 // CONTROLLERS IMPORTS
-const analyseCarImage = require('../controllers/controller');
+const analyseCarImage = require('../controllers/analyseCar');
 const calculateCarValue = require('../controllers/carValueController')
 const calculateRiskRating = require('../controllers/riskRatingController')
 const calculateQuote = require('../controllers/calculateQuote')
@@ -15,9 +15,9 @@ const calculateQuote = require('../controllers/calculateQuote')
 // ROUTES
 router.get('/', (req, res) => res.send('server is up'));
 router.post('/analyse-car-image', upload.single('car-image'), (req, res) => analyseCarImage(req, res));
-app.post('/calculate_car_value', calculateCarValue);
-app.post('/calculate_risk_rating', calculateRiskRating);
-app.post('/calculate_quote', calculateQuote);
+router.post('/calculate_car_value', calculateCarValue);
+router.post('/calculate_risk_rating', calculateRiskRating);
+router.post('/calculate_quote', calculateQuote);
 
 // EXPORT ROUTER
 module.exports = router;
